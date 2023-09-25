aaf4d49b762600cced6c6f7fc27b66a140e9fac5
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-25 17:45:41 +0300

    add

--- scripts/index.js
+++ scripts/index.js
@@ -14,12 +14,17 @@ function show_password(TARGET) {
 // start point
 document.addEventListener("DOMContentLoaded", ()=>{
     // get element from document
-    const LEFT_BLOCK = document.querySelector(".form__top_LFor_Lblock")
-    const RIGHT_BLOCK = document.querySelector(".form__top_LFor_Rblock")
-    const LF_BLOCK = document.querySelector(".form__top_LFor")
+    const LEFT_BLOCK = document.querySelector(".form__top_LFor_Lblock");
+    const RIGHT_BLOCK = document.querySelector(".form__top_LFor_Rblock");
+    const LF_BLOCK = document.querySelector(".form__top_LFor");
+    const CHECK_BOX = document.querySelector(".form__input_checkbox");
     // switch user role
     LF_BLOCK.addEventListener("click", ()=>{
-        LEFT_BLOCK.classList.toggle('active')
-        RIGHT_BLOCK.classList.toggle('active')
+        LEFT_BLOCK.classList.toggle('active');
+        RIGHT_BLOCK.classList.toggle('active');
+    })
+    // get value form checkbox on click
+    CHECK_BOX.addEventListener("click", function(e){
+        alert('test')
     })    
 })
