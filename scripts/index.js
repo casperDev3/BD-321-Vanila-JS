e4f8ee237445fb64f43458961ea5a938a480fbc8
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-09-25 18:08:24 +0300

    add part ToS button

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 function show_password(TARGET) {
     console.log(TARGET.type)
     const PASSWORD = document.getElementById(TARGET);
-    if(PASSWORD.type === "password") {
+    if (PASSWORD.type === "password") {
         PASSWORD.type = "text";
-    } 
+    }
     else {
         PASSWORD.type = "password";
     }
-  }
+}
 
 
 
 // start point
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     // get element from document
     const LEFT_BLOCK = document.querySelector(".form__top_LFor_Lblock");
     const RIGHT_BLOCK = document.querySelector(".form__top_LFor_Rblock");
     const LF_BLOCK = document.querySelector(".form__top_LFor");
     const CHECK_BOX = document.querySelector(".form__input_checkbox");
+    const SUBMIT_BTN = document.querySelector(".form__input_sumbit")
     // switch user role
-    LF_BLOCK.addEventListener("click", ()=>{
+    LF_BLOCK.addEventListener("click", () => {
         LEFT_BLOCK.classList.toggle('active');
         RIGHT_BLOCK.classList.toggle('active');
     })
     // get value form checkbox on click
-    CHECK_BOX.addEventListener("click", function(e){
-        alert('test')
-    })    
+    CHECK_BOX.addEventListener("click", function (e) {
+        const CHECK_BOX_VALUE = CHECK_BOX.checked
+        // CHECK_BOX_VALUE ? SUBMIT_BTN.disabled = !CHECK_BOX_VALUE : SUBMIT_BTN.disabled = !CHECK_BOX_VALUE;
+        CHECK_BOX_VALUE ? SUBMIT_BTN.disabled = false : SUBMIT_BTN.disabled = true;
+    })
 })
