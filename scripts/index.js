b008692d2f1915619b59dd7686ea346d43619fc6
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-10-06 17:44:25 +0300

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
-$(document).ready((e)=>{
-    alert("test!")
+// $(document).ready((e)=>{
+//     alert("test!")
+// })
+
+// click
+$(".jQ_click").click((e)=>{
+//    $("body").toggleClass("active");
+//    console.log($(".jQ_click").data("id"))
+//    console.log($(".jQ_click").attr("data-id"))
+$("body").append("<h2>HELLO WORLD!</h2>")
 })
+
