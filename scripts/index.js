94939b8f543922d1127e927bb7bdc09b2b40798a
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-10-09 19:20:34 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,23 @@
 function turnOnErrorView(selector_input, selector_msg, msg_text) {
+    console.log("test Err")
     document.querySelector(selector_input).classList.add("error")
     document.querySelector(selector_msg).classList.add("error")
     document.querySelector(selector_msg).innerHTML = msg_text;
 }
 
+function turnOffErrorView(selector_input, selector_msg){
+    console.log("test")
+    document.querySelector(selector_input).classList.remove("error")
+    document.querySelector(selector_msg).classList.remove("error")
+}
+
 function getUserData() {
     // get data
-    let userName = document.querySelector(".form__name").value 
-    || turnOnErrorView(".form__name", ".form__name_error", "U don't write name!");
+    let userName = document.querySelector(".form__name").value
+    userName ? turnOffErrorView(".form__name", ".form__name_error")
+    : turnOnErrorView(".form__name", ".form__name_error", "U don't write name!")
+   
+    
 }
 
 
