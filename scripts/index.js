2d02170704dee5852bf30ab14dbb18ec424038fd
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-10-16 19:51:22 +0300

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-alert("test");
+// function displayInfo(name: string, age: number): void{
+//     console.log(`Name: ${name}, age: ${age}`)
+// }
+var displayInfo = function (name, age) {
+    console.log("Name: ".concat(name, ", age: ").concat(age));
+};
+var userName = "John";
+var age = 15;
+displayInfo(userName, age);
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,1 +1,33 @@
-alert("test")
+// function displayInfo(name: string, age: number): void{
+//     console.log(`Name: ${name}, age: ${age}`)
+// }
+
+const displayInfo = (name: string, age: number): void => {
+    console.log(`Name: ${name}, age: ${age}`)
+}
+
+interface userDataTypes {
+    name: string,
+    age: number
+}
+
+
+
+let userName : any = "John";
+let age : number = 15;
+let isAdult : boolean = age > 18;
+
+let test2: unknown = 12;
+
+let teswt3: null = null;
+let test4: undefined = undefined;
+
+let userData: userDataTypes = {
+    name: "John",
+    age: 21
+}
+
+userData.name = "test"
+
+
+displayInfo(userName, age)
